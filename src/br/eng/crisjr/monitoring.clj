2aed35e84cc84fa2b3e22eb1143799f84a5b0031
Cris Silva Jr <crisjr@pm.me>
2022-09-28 14:24:56 -0300

    Refactor ifconfig code

--- src/br/eng/crisjr/monitoring.clj
+++ src/br/eng/crisjr/monitoring.clj
@@ -2,17 +2,17 @@
   (:require [clojure.java.shell :refer [sh]]
             [clojure.string :as string]))
 
-
 (defn get-ip-address []
-  ; TODO complete me!
-  (let [ifconfig (:out (sh "ifconfig"))
-        lines (string/split-lines ifconfig)
-        limit (count lines)]
-    (loop [n 0]
-      (if (= n limit) 
-        nil
-        (let [line (nth lines n)
-              maybe-match (re-find #"inet 192\.168\.\d+\.\d+" line)]
-          (if (nil? maybe-match)
-            (recur (inc n))
-            (second (string/split maybe-match #"\s"))))))))
+  (reduce (fn [result line]
+            (or result
+                (let [maybe-match (re-find #"inet 192\.168\.\d+\.\d+" line)]
+                  (if (nil? maybe-match)
+                    nil
+                    (-> maybe-match
+                        (string/split #"\s")
+                        second)))))
+          nil
+          (-> "ifconfig"
+              sh
+              :out
+              string/split-lines)))
