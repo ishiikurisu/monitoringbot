e212d0d07f397bed9d0ae61757271b133f6e0cb3
Cris Silva Jr <crisjr@pm.me>
2022-09-28 14:16:16 -0300

    Get IP address from `ifconfig`

--- src/br/eng/crisjr/monitoring.clj
+++ src/br/eng/crisjr/monitoring.clj
@@ -1,8 +1,18 @@
 (ns br.eng.crisjr.monitoring
-  (:require [clojure.java.shell :refer [sh]]))
+  (:require [clojure.java.shell :refer [sh]]
+            [clojure.string :as string]))
+
 
 (defn get-ip-address []
   ; TODO complete me!
-  (let [ifconfig (:out (sh "ifconfig"))]
-    (println ifconfig)
-    "77.77.77.77"))
+  (let [ifconfig (:out (sh "ifconfig"))
+        lines (string/split-lines ifconfig)
+        limit (count lines)]
+    (loop [n 0]
+      (if (= n limit) 
+        nil
+        (let [line (nth lines n)
+              maybe-match (re-find #"inet 192\.168\.\d+\.\d+" line)]
+          (if (nil? maybe-match)
+            (recur (inc n))
+            (second (string/split maybe-match #"\s"))))))))
